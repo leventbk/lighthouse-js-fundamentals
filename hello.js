679e7ae6ae27519bcd49da92b6a276ae965a7ce5
Levent Karakus <leventbkarakus@hotmail.com>
2023-03-31 14:06:21 -0400

    funtion that print hello with a name as a parameter

---
+++ hello.js
@@ -0,0 +1,9 @@
+// const sayHello  = function () {
+// 	console.log("Hello, world");
+//   }
+  
+const sayHello  = function (name) {
+	console.log("Hello, " + name);
+  }
+
+  sayHello("levent");  sayHello("bahadirhan");  sayHello("karakus");
