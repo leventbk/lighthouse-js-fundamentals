e5351027d527cafac5fc73605ad4ff1442d90f09
Levent Karakus <leventbkarakus@hotmail.com>
2023-03-31 14:26:37 -0400

    isEven  function with its call

---
+++ even.js
@@ -0,0 +1,18 @@
+const isEven = function (num) {
+	return num %2 ===0 ;
+	
+}
+
+// const callIsEven = function (isEven) {
+// 	if (isEven) {
+// 		console.log(isEven +' Yay! the number is even!')
+// 	}else {
+// 		console.log('Yah! It isn\'t even number');
+// 	}
+// }
+// callIsEven('asdf');
+
+const tenIsEven = isEven(10);
+const nineIsEven = isEven(9);
+console.log(tenIsEven);
+console.log(nineIsEven);
