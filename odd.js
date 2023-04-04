28b3857c4e043f98f761e42f12edcca237fd87ed
Levent Karakus <leventbkarakus@hotmail.com>
2023-04-04 12:23:09 -0400

    odd checker with 2 different function

---
+++ odd.js
@@ -0,0 +1,22 @@
+// this function return always true
+function isOdd(num) {
+	if (num %2 === 0) {
+		return false
+	} else{return true}
+}
+
+//alternative solution
+function isOddAlt(numAlt) {
+	return numAlt % 2 !== 0;
+}
+
+
+console.log(isOdd(1))
+console.log(isOdd(2))
+console.log(isOdd(3))
+console.log(isOdd(4))
+console.log('-------')
+console.log(isOddAlt(1))
+console.log(isOddAlt(2))
+console.log(isOddAlt(3))
+console.log(isOddAlt(4))
