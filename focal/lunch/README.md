1e1aef6edfd482119d25d64772dadbb81a11deb8
leventkarakus <leventbkarakus@hotmail.com>
2023-05-01 22:01:46 -0400

    w1d1

---
+++ focal/lunch/README.md
@@ -0,0 +1,7 @@
+# What should I Do for Lunch tips
+## Generic comparison operators in the node REPL
+
+Include 2 parameter to decision by boolean and number
+```javascript
+function whatToDoForLunch(hungry, availableTime);
+```
--- focal/lunch/lunch.js
+++ focal/lunch/lunch.js
@@ -14,23 +14,22 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  if ( !hungry) {
-    console.log('im not hungry'); 
-  } 
-  else if (availableTime >= 30 ) {
+  if (!hungry) {
+    console.log('im not hungry');
+  } else if (availableTime >= 30) {
     console.log('eat it in the lab');
   } else if (availableTime <= 30 && availableTime >= 20) {
     console.log('try a place nearby');
   } else if (hungry && availableTime < 20) {
     console.log('we want to remind ourselves that we are in a bootcamp and that we should reconsider how much time we actually have to spare');
-  } 
+  }
   console.log("I don't know what to do!");
-}
+};
 
 
 /*
  * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
+ * defied above to verify we're making the right decisions. Do not modify it!
  */
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
